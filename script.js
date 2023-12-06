48fac12d6881e3ba1b069b65deb55af3c73dff11
Saswata Paul <saspaul2014@gmail.com>
2023-12-07 01:42:46 +0530

    Added the button functionality and now the text content of the button also changes based on their turns

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function clickDetector(num)
+let Xturn = 0;
+let Xchoice = [];
+let Ochoice = [];
+
+function getPlayerChoice(num) 
 {
-    console.log(num);
+    if (Xturn === 0) 
+    {
+        Xchoice.push(num);
+        console.log("X choice: " + Xchoice);
+        updateXPlay(num);
+        Xturn = 1;
+    } 
+    else 
+    {
+        Ochoice.push(num);
+        console.log("O choice: " + Ochoice);
+        updateOPlay(num);
+        Xturn = 0;
+    }
+}
+
+function updateXPlay(num) 
+{
+    var XbuttonElement = document.querySelector(`.btn-${num}`);
+    XbuttonElement.textContent = "X";
+}
+
+function updateOPlay(num)
+{
+    var ObuttonElement = document.querySelector(`.btn-${num}`);
+    ObuttonElement.textContent = "O";
 }
